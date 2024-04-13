04c3a74c6fc89f2483e81a413526b8a462891cb8
B21DCCN484-Nguyễn Khánh Linh <143245545+lcsiucute@users.noreply.github.com>
2024-04-13 04:31:30 +0000

    new

--- projects/project4/webServer.js
+++ projects/project4/webServer.js
@@ -17,9 +17,74 @@ var portno = 3000;   // Port number to use
 
 var app = express();
 
+var cs142models = require('./modelData/photoApp.js').cs142models;
+
 // We have the express static module (http://expressjs.com/en/starter/static-files.html) do all
 // the work for us.
 app.use(express.static(__dirname));
+
+app.get('/', function (request, response) {
+  response.send('Simple web server of files from ' + __dirname);
+});
+
+app.get('/test/:p1', function (request, response) {
+  // Express parses the ":p1" from the URL and returns it in the request.params objects.
+  var param = request.params.p1;
+  console.log('/test called with param1 = ', param);
+  if (param !== "info") {
+    console.error("Nothing to be done for param: ", param);
+    response.status(400).send('Not found');
+    return;
+  }
+  
+  var info = cs142models.schemaInfo();
+  
+  // Query didn't return an error but didn't find the SchemaInfo object - This
+  // is also an internal error return.
+  if (info.length === 0) {
+    response.status(500).send('Missing SchemaInfo');
+    return;
+  }
+  response.status(200).send(info);
+});
+
+/*
+ * URL /user/list - Return all the User object.
+ */
+app.get('/user/list', function (request, response) {
+  response.status(200).send(cs142models.userListModel());
+  return;
+});
+
+/*
+ * URL /user/:id - Return the information for User (id)
+ */
+app.get('/user/:id', function (request, response) {
+  var id = request.params.id;
+  var user = cs142models.userModel(id);
+  if (user === null) {
+    console.log('User with _id:' + id + ' not found.');
+    response.status(400).send('Not found');
+    return;
+  }
+  response.status(200).send(user);
+  return;
+});
+
+/*
+ * URL /photosOfUser/:id - Return the Photos for User (id)
+ */
+app.get('/photosOfUser/:id', function (request, response) {
+  var id = request.params.id;
+  var photos = cs142models.photoOfUserModel(id);
+  if (photos.length === 0) {
+    console.log('Photos for user with _id:' + id + ' not found.');
+    response.status(400).send('Not found');
+    return;
+  }
+  response.status(200).send(photos);
+});
+
 
 var server = app.listen(portno, function () {
   var port = server.address().port;
